3254cfd7efaa8676fb600d081ad1375cc521caee
Anil Bamnote <anilbamnote21@gmail.com>
2025-11-24 16:45:21 +0530

    test-stage-added

--- pileline.groovy
+++ pileline.groovy
@@ -13,10 +13,10 @@ pipeline {
         }
         stage('test') {
             steps {
-            //     withSonarQubeEnv(installationName: 'sonar',credentialsId: 'sonar-cred') {
-            //        sh '/opt/maven/bin/mvn sonar:sonar'
-            //    }
-                     sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://35.74.27.54:9000  -Dsonar.login=9c547e0733545b5a64a73f56e83c0169e6b5e1d7'
+                withSonarQubeEnv(installationName: 'sonar',credentialsId: 'sonar-cred',variable: 'SONAR_CRED') {
+                   sh '/opt/maven/bin/mvn sonar:sonar'
+               }
+                    //  sh '/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://35.74.27.54:9000  -Dsonar.login=9c547e0733545b5a64a73f56e83c0169e6b5e1d7'
             }
         }
         stage('deploy') {
