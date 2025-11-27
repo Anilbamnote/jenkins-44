c4ef19d9e915d84956152791fda7bcdd3e9b42a8
Anil Bamnote <anilbamnote21@gmail.com>
2025-11-27 15:49:41 +0530

    test-stage-added

--- pileline.groovy
+++ pileline.groovy
@@ -26,11 +26,16 @@ pipeline {
         //         waitForQualityGate true
         //     }
         // }
-        stage('deploy') {
+         stage('s3-artifactory') {
             steps {
-                deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://35.78.123.34:8080')], contextPath: '/', war: '**/*.war'
+                sh 'aws s3 cp target/studentapp-2.2-SNAPSHOT.war  s3://docker-buck-artifact-0099955'
             }
         }
+        // stage('deploy') {
+        //     steps {
+        //         deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://35.78.123.34:8080')], contextPath: '/', war: '**/*.war'
+        //     }
+        // }
     }
 }
 
